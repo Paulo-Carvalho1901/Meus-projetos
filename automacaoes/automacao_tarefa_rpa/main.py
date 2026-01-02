59798e2c18db2b2591f6817432d29c9fd0be7043
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-02 16:27:11 -0300

    feat: concluido automação de arquivo

--- automacaoes/automacao_tarefa_rpa/main.py
+++ automacaoes/automacao_tarefa_rpa/main.py
@@ -3,15 +3,35 @@
 
 
 import pyautogui
+import time
 
 pyautogui.alert('O código vai começar, Não mexa em nada!')
-pyautogui.PAUSE = 1.5
+pyautogui.PAUSE = 2.5
 
-# Abrir o Google Drive no meu computador
+# fe
 pyautogui.press('winleft')
-pyautogui.write('chrome')
+pyautogui.write('onedrive')
 pyautogui.press('enter')
 
 # chaves = pyautogui.KEYBOARD_KEYS
 # print(chave)
 
+# entrando na area de trabalho
+pyautogui.hotkey('winleft', 'd')
+
+# posição do mouse
+# print(pyautogui.position()) # descobrir a posição onde está seu mouse
+
+# pyautogui.position()
+pyautogui.moveTo(x=727, y=529)
+
+# Precionando botão do mouse esquerdo
+pyautogui.mouseDown()
+
+# movendo arquivo
+pyautogui.moveTo(x=869, y=361)
+pyautogui.hotkey('alt', 'tab')
+time.sleep(1)
+pyautogui.mouseUp()
+
+pyautogui.alert('Terminou')
