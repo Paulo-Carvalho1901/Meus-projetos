63dc0d2e16a226bd55bc72a1fe0820ecda1061cb
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-02 15:25:04 -0300

    feat: automação Abrir o Google Drive no meu computador

---
+++ automacaoes/automacao_tarefa_rpa/main.py
@@ -0,0 +1,17 @@
+# Documentação pyautoGUI
+# https://pyautogui.readthedocs.io/en/latest/install.html
+
+
+import pyautogui
+
+pyautogui.alert('O código vai começar, Não mexa em nada!')
+pyautogui.PAUSE = 1.5
+
+# Abrir o Google Drive no meu computador
+pyautogui.press('winleft')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+
+# chaves = pyautogui.KEYBOARD_KEYS
+# print(chave)
+
