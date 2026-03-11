9e246bfdd7babfb5ed70195cbb31ba7a3d5e27a5
Gabriel Brognoli <gabriel.brognoli@global.ntt>
2026-03-11 01:26:13 -0300

    adiciona conteúdo inicial do curso de Python

---
+++ Estudo_python/modulo_01_iniciante/aula02.py
@@ -0,0 +1,39 @@
+# ==========================================================
+# Aula 02 - Variáveis
+# ==========================================================
+
+
+# Variáveis são espaços na memória que guardam dados.
+
+
+# Criando variáveis
+
+nome = "Gabriel"
+idade = 38
+cidade = "Brasília"
+
+
+# Podemos mostrar o valor de uma variável usando print()
+
+print(nome)
+print(idade)
+print(cidade)
+
+
+# Também podemos juntar texto com variáveis
+
+print("Meu nome é", nome)
+print("Tenho", idade, "anos")
+print("Moro em", cidade)
+
+
+# ==========================================================
+# EXERCÍCIO
+#
+# Crie variáveis com seus dados:
+# nome
+# idade
+# cidade
+#
+# Depois mostre uma frase com essas informações.
+# ==========================================================
