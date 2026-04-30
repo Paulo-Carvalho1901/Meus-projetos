d33c8a6dd44cf27ca9997b8eaf2151f2dc50e452
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-04-30 15:06:51 -0300

    (ex1) concluido o exercicio soma do valores

---
+++ python_basico/Lista_exercicio/ex01.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que lei dois números
+e mostre a soma entre eles
+"""
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+soma = n1 + n2
+print(f'A soma entre {n1} + {n2} = {soma}')
