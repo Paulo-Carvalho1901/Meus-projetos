013e1e90f702880b9bb8bdf6c1e1f65f372efa50
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-04-30 13:43:05 -0300

    (aula) tipos primitivos basico em Python

---
+++ python_basico/modulo_1/aula1_tipos_primitivos.py
@@ -0,0 +1,17 @@
+# Criando variavel somandos dois números
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+soma = n1 + n2
+print('A soma é:', soma)
+
+"""
+Os tipos primitivos mais basicos
+
+int - inteiros - 7, 5, 0, 1, -1, -10
+float - pontos flutuantes - 10.15, 10.0, 7.5, -10.5
+bool - lógicos - True ou False
+str - caractere ou string (tipo texto)
+"""
+
