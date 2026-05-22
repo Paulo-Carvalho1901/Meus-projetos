2c0eadd77780634a525f1f679a6d242c365e21d0
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-22 15:37:26 -0300

    (operador aritimetico) entendendo operadores

---
+++ python_basico/modulo_1/aula2_operadores_aritimeticos.py
@@ -0,0 +1,19 @@
+# Operadores aritiméricos
+
+"""
++ adição
+- subtração
+* multiplicação
+/ Divisão
+** potencia
+// divisão inteira
+% moduo ou resto
+"""
+
+"""
+Ordem de precedencia
+1.()
+2.**
+3.* / // %
+4 + -
+"""
