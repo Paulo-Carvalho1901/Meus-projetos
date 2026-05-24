34f31c81a44e3dcc8f67b8820258b46bae03a502
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-23 21:25:35 -0300

    (ex) entendendo exercicio sobre os numeros

--- python_basico/modulo_1/aula2_operadores_aritimeticos.py
+++ python_basico/modulo_1/aula2_operadores_aritimeticos.py
@@ -34,3 +34,16 @@ print()
 
 nome = input('Qual é seu nome: ')
 print(f'É uma prazer te conhecer {nome:=^34}!')
+
+print()
+print('=' * 30)
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print(f'A soma é {s}, o produto é {m} e a divisão é {d:.3}', end=' ')
+print(f'A divisão inteira é {di} e potencia é {e}')
