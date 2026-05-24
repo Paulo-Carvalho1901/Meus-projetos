67530cb0b1649e783cffed036e6d1da99ee0894a
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-24 20:55:01 -0300

    (operadores) calculadora no Python

---
+++ python_basico/Lab/lab1_python_calculadora.py
@@ -0,0 +1,40 @@
+# Python como uma calculadora
+
+print(2 + 2)
+print(2 / 2)
+print(2 - 2)
+print(2 / 3)
+print(2 // 3)
+print(7 % 2)
+print(5 ** 2)
+
+# Exponenciação
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+# Multiplicação
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+# Divisão
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+# Divisão de número inteiro (divisão arredondada)
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+print(6 // 4)
+print(6. // 4)
+
+# Teste
+print((2 ** 4), (2 * 4.), (2 * 4))
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
