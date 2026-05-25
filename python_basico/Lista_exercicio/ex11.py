955e039693a7089d85ca2f6e26cc828e76b2806c
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 16:19:16 -0300

    (ex10) salario com 15% de aumento

---
+++ python_basico/Lista_exercicio/ex11.py
@@ -0,0 +1,10 @@
+"""
+Faça um algoritmo que leia
+o salário de um funcionario e mostre
+seu novo salário, com 15% de aumento.
+"""
+
+salario = float(input('Digite seu salario: '))
+
+salario_aumento = salario * 1.15
+print(f'Seu salario éra R$ {salario}, agora seu salário com 15% de aumento é R$ {salario_aumento}')
