c0a84dea73f006a5fbbe31b069c0964f05911c07
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 01:06:21 -0300

    (ex6) convertendo metro em cm e mm

---
+++ python_basico/Lista_exercicio/ex06.py
@@ -0,0 +1,10 @@
+"""
+Escreva um programa que leia um
+valor em metros e o exiba convertido em centimentros
+e milimetros.
+"""
+
+valor = int(input('Digite o valor em metros: '))
+
+print(f'O valor de metros foi {valor}m e em cintimentro é {valor * 100}cm')
+print(f'O valor em metros foi {valor}m e em milimetro é {valor * 1000}mm')
