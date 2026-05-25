9b6e2a8b5c2a19a930d1fd56b308454d5bedddf8
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 00:54:03 -0300

    (ex4) resolvido problema do dobro, triplo e raiz

---
+++ python_basico/Lista_exercicio/ex04.py
@@ -0,0 +1,10 @@
+"""
+Crie um algoritmo que leia um número e mostre
+o seu dubro, tiplo, e raiz quadrada.
+"""
+
+numero = int(input('Digite um número: '))
+
+print(f'Seu dobro é {numero * 2}')
+print(f'Seu triplo é {numero * 3}')
+print(f'Sua raiz é {numero ** (1/2)}')
