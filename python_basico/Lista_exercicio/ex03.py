3ce4bc4487d4ded34e913704ecc1d1e2e42fec08
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 00:51:13 -0300

    (ex3) numero antecessor e sucessor

---
+++ python_basico/Lista_exercicio/ex03.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que leia um numero inteiro
+e mostre na tela o seu sucessor e seu antecessor
+"""
+numero = int(input('Digite um numero: '))
+
+print(f'O seu número é {numero}, seu antecessor é {numero - 1} e seu sucessor é {numero + 1}')
