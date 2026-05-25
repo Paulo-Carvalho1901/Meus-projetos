de2d437da63cb2800a86fc89541b42b5ab3b965c
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 01:00:34 -0300

    (ex5) calculando media do aluno

---
+++ python_basico/Lista_exercicio/ex05.py
@@ -0,0 +1,13 @@
+"""
+Desenvolva um programa que leia
+as duas notas de um aluno, calcule e
+mostre sua média.
+"""
+
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+print(f'Primeira nota foi {nota1}')
+print(f'Segunda nota nota foi {nota2}')
+print(f'A média do aluno é {media}')
