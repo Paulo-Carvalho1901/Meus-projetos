0824ed6d4688f8ebeb5837828dcc6937e6ba5685
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 01:14:14 -0300

    (ex7) criado calculo tabuada

---
+++ python_basico/Lista_exercicio/ex07.py
@@ -0,0 +1,19 @@
+"""
+Faça um programa que leia um número
+inteiro qualquer e mostre na tela
+a sua tabuada.
+"""
+
+valor = int(input('Digite um número: '))
+
+print(f'{valor} x 0 = {valor * 0}')
+print(f'{valor} x 1 = {valor * 1}')
+print(f'{valor} x 2 = {valor * 2}')
+print(f'{valor} x 3 = {valor * 3}')
+print(f'{valor} x 4 = {valor * 4}')
+print(f'{valor} x 5 = {valor * 5}')
+print(f'{valor} x 6 = {valor * 6}')
+print(f'{valor} x 7 = {valor * 7}')
+print(f'{valor} x 8 = {valor * 8}')
+print(f'{valor} x 9 = {valor * 9}')
+print(f'{valor} x 10 = {valor * 10}')
