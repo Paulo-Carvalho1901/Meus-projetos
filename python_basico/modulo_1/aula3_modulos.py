4b897225ef47f41bc68e8a61f927e255b8a085a9
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 23:22:05 -0300

    (random) randomizando numeros inteiros

--- python_basico/modulo_1/aula3_modulos.py
+++ python_basico/modulo_1/aula3_modulos.py
@@ -1,7 +1,14 @@
 # import math
 from math import sqrt, ceil
+import random
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
 
 print(f'A raiz de {num} é igual á {raiz:.2f}') # setando apenas 2 numero após a vingula
-print(f'A raiz de {num} é igual á {ceil(raiz)}') # arredondando
+print(f'A raiz de {num} é igual á {ceil(raiz)}') # arredondando para cima
+
+print('=' * 45)
+
+# Gerando numeros aliatórios
+numero = random.randint(1, 10)
+print(numero)
