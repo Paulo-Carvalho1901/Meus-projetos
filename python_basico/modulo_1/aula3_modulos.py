bca0e498d91d631b2846a3c1eeee19c967db225a
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-25 23:17:23 -0300

    (math) fazendo uma importação personalizada

--- python_basico/modulo_1/aula3_modulos.py
+++ python_basico/modulo_1/aula3_modulos.py
@@ -1,6 +1,7 @@
-import math
+# import math
+from math import sqrt, ceil
 num = int(input('Digite um número: '))
-raiz = math.sqrt(num)
+raiz = sqrt(num)
 
 print(f'A raiz de {num} é igual á {raiz:.2f}') # setando apenas 2 numero após a vingula
-print(f'A raiz de {num} é igual á {math.ceil(raiz)}') # arredondando
+print(f'A raiz de {num} é igual á {ceil(raiz)}') # arredondando
