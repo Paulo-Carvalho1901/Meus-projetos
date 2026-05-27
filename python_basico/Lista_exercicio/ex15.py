88b98b6f2e41e34a66b3114748cfd7e8473c43cd
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-27 00:31:22 -0300

    (ex15) calculo com importaçao

--- python_basico/Lista_exercicio/ex15.py
+++ python_basico/Lista_exercicio/ex15.py
@@ -3,7 +3,18 @@
 # triângulo retângulo. Calcule e mostre comprimento
 # da hipotenusa
 
+# Calculo matematico da hipotenusa
 co = float(input('Comprimento do coteto aposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 hi = (co ** 2 + ca ** 2) ** (1/2)
 print(f'A hipotenusa vai medir {hi:.2f}')
+
+print('=' * 40)
+
+print('CALCULO COM IMPORTAÇÃO')
+# calculo com importação
+import math
+co_2 = float(input('Comprimento do cateto aposto: '))
+ca_2 = float(input('Comprimento do cateto adjacente: '))
+hi_2 = math.hypot(co_2, ca_2)
+print(f'A hipotenusa vai medir {hi_2:.2f}')
