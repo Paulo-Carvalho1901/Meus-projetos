4c9502286c18f8a079275d0366881e7063af2c8b
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-27 18:03:25 -0300

    (ex16) calculando o seno

---
+++ python_basico/Lista_exercicio/ex16.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um ângulo qualquer
+# e mostre na tela o valor da seno, cosseno e tangente
+# desse ângulos.
+
+import math
+
+angulo = float(input('Digite o ângulo: '))
+seno = math.sin(math.radians(angulo)) # Calculando o seno do ângulo
+
