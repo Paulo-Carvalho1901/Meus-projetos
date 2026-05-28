cfb9140444327c1033dad4affea562b4afb07151
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-28 17:52:57 -0300

    adicionando uma docsting com texto em multilinhas

--- python_basico/modulo_1/aula4_manipulando_texto.py
+++ python_basico/modulo_1/aula4_manipulando_texto.py
@@ -1,5 +1,13 @@
 # Manipulando strings em Python
 
+print("""
+Welcome! Are you completely new to programming?
+about why and how to get started with Python. Fortunately
+an experienced programmer in any programming language
+(whatever is may be) can pick up Python very quickly.
+Its also easy for beginner to use and learn, so junp in!
+""")
+
 # Fatiamento
 frase = 'Curso em Video Python'
 frase_2 ='   Aprenda Python   '
