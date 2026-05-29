d0d81c7622cb61c82187a4f013fa0b558ff72539
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-05-29 00:39:59 -0300

    (projeto1) lendo a tabela

--- projetos/projetos_01/app.py
+++ projetos/projetos_01/app.py
@@ -6,6 +6,11 @@ import openpyxl
 
 
 # Ler um arquivo do excel
-workbook = openpyxl.load_workbook('Base_dados_produtos_reais.xlxs')
+workbook = openpyxl.load_workbook('Base_dados_produtos_reais.xlsx')
 vendas_sheet = workbook['vendas'] # selecionando a pagina para trabalhar
 
+for linha in vendas_sheet.iter_rows(min_row=2):
+    print(linha[0].value)
+    print(linha[1].value)
+    print(linha[2].value)
+    print(linha[3].value)
