a9b4d9ce94f779090b1ab721baa60863f10b1aa6
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-06-03 17:20:47 -0300

    inicio de app do projeto de web scraping

---
+++ projetos/projeto_02-automacao-web-com-selenium/app.py
@@ -0,0 +1,7 @@
+# Preencher um fomulario na internet
+
+# Passo 1: Entrar https://pages.hashtagtreinamentos.com/arquivo-python-1jGh7kZSxQLoznA_GgwnWa8f7LEl3kKCZ?origemurl=hashtag_yt_org_planilhapyt_8AMNaVt0z_M
+# Passo 2: Preencher nome, preencher email
+# Clicar no botão enviar formulariow
+
+
