ea7f7372f76af1302614c5349c84810f25a7964a
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-06-05 16:49:08 -0300

    links para explicação do ETL

---
+++ Scrapper_Python/links.py
@@ -0,0 +1,3 @@
+# ETL - (zendesk) https://www.zendesk.com.br/blog/employee-service/itsm/o-que-e-etl/
+# Data Science Academy - https://blog.dsacademy.com.br/mas-afinal-o-que-e-pipeline-de-dados/
+# DigitalOcean - https://www.digitalocean.com/community/tutorials/como-fazer-scraping-em-paginas-web-com-beautiful-soup-and-python-3-pt
