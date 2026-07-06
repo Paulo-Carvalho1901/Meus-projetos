648cc1f3c793edd771a6948ac9e784dbdc087594
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-06 10:49:24 -0300

    add .gitignore atualizado

--- ETL/links.py
+++
@@ -1,3 +0,0 @@
-# ETL - (zendesk) https://www.zendesk.com.br/blog/employee-service/itsm/o-que-e-etl/
-# Data Science Academy - https://blog.dsacademy.com.br/mas-afinal-o-que-e-pipeline-de-dados/
-# DigitalOcean - https://www.digitalocean.com/community/tutorials/como-fazer-scraping-em-paginas-web-com-beautiful-soup-and-python-3-pt
