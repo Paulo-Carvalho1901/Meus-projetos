c8e23103f4714c79202419e9c59cec484f031ce9
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-06 18:14:29 -0300

    trabalhando com concatenação de valores em Python

---
+++ Python_zero_a_inteligecia_artificial_2026/modulo_01_fundamentos/aula04_concatenacao.py
@@ -0,0 +1,11 @@
+# Trabalahndo com concatenação de valores
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura: '))
+ativo = True
+
+print(nome, type(nome))
+print(idade, type(idade))
+print(altura, type(altura))
+print(ativo, type(ativo))
