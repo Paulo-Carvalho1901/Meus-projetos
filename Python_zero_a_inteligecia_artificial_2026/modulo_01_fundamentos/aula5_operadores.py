73a835589ac13368eb1cc786542bd78f60651630
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-07 14:33:13 -0300

    trabalhando com operadores aritiméticos

---
+++ Python_zero_a_inteligecia_artificial_2026/modulo_01_fundamentos/aula5_operadores.py
@@ -0,0 +1,14 @@
+# Trabalhando com operadores em Python
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+# Aritiméticos
+soma = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2 # resto da divisão
+exp = num1 ** num2
+
+print(soma)
