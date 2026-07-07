c52e37c7982c6d6b15dfa8f6039d4cc2f5dd9dd9
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-07 14:53:31 -0300

    apagado inicio do curso ruin de mais

--- Python_zero_a_inteligecia_artificial_2026/modulo_01_fundamentos/aula01_primeiro_programa.py
+++
@@ -1,9 +0,0 @@
-print(5 + 5) # imprimindo uma mensagem na tela
-print('Ola mundo!')
-print("Ola mundo!")
-# isso é comentario
-"""
-Esse comenatario
-é em varias linhas
-Python é muito bom!
-"""
--- Python_zero_a_inteligecia_artificial_2026/modulo_01_fundamentos/aula03_input.py
+++
@@ -1,9 +0,0 @@
-# Trabalhando com input
-
-nome = input('Digite o nome do filme: ')
-ano_lancamento = int(input('Digite o ano de lançamento do filme '))
-nota_do_filme = float(input('Digite a nota do filme: '))
-
-print(nome, type(nome))
-print(ano_lancamento, type(ano_lancamento))
-print(nota_do_filme, type(nota_do_filme))
