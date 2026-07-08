79cf38d7b66725fd36d270ac97068370c71353a5
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-08 17:53:35 -0300

    validando nome da cidade

--- python_basico/Lista_exercicio/ex21.py
+++ python_basico/Lista_exercicio/ex21.py
@@ -3,3 +3,8 @@ Crie um programa que leia o
 nome de uma cidade e diga se ela
 começa com o nome "SANTO"
 """
+
+nome_cidade = input('Digite da sua cidade: ')
+primeiro_nome_cidade = nome_cidade.split()[0]
+
+print('O nome da cidade começa com Santo', primeiro_nome_cidade in 'Santo')
