0333f901fc022a04d6eebe04a95fa97f16be7914
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-08 13:32:59 -0300

    calculo das centena

--- python_basico/Lista_exercicio/ex20.py
+++ python_basico/Lista_exercicio/ex20.py
@@ -16,6 +16,9 @@ numero = int(input('Digite um número de 0 a 9999: '))
 
 unidade = numero % 10
 dezena = (numero // 10) % 10
+centena = (numero // 100) % 10
+
 
 print(unidade)
 print(dezena)
+print(centena)
