261bcc09e2735c9a884e6d77888f17c59cd6287e
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-08 13:10:01 -0300

    ex19: nome com letras maiuscula

--- python_basico/Lista_exercicio/ex19.py
+++ python_basico/Lista_exercicio/ex19.py
@@ -11,3 +11,6 @@ Quantas letras ao todo sem considerar os espaços
 Quantas letras tem o primeiro nome
 """
 
+nome_completo = input('Digite seu nome completo: ')
+
+print(f'Seu nome todas as letras maiuscula {nome_completo.upper()}')
