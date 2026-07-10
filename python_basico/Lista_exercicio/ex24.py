01250697f52dc1251d3c9b49ddd7e45391d38ab4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-10 17:56:42 -0300

    selecionando o primeiro e ultimo nome pelos index de uma lista

--- python_basico/Lista_exercicio/ex24.py
+++ python_basico/Lista_exercicio/ex24.py
@@ -7,3 +7,10 @@ ex: Ana Maria de Souza
 Primeiro: Ana
 Segundo: Maria
 """
+nome_completo = input('Digite seu nome: ')
+primeiro_nome = nome_completo.split()[0]
+ultimo_nome = nome_completo.split()[-1]
+
+print(f'Seu primeiro nome é {primeiro_nome}')
+print(f'Seu ultimo nome é {ultimo_nome}')
+
