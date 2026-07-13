e8557f3d0453ebd8f7963889a0bb6b202dabccd6
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-07-13 17:04:54 -0300

    calculo de media

--- python_basico/modulo_1/aula5_condicoes_parte_01.py
+++ python_basico/modulo_1/aula5_condicoes_parte_01.py
@@ -7,9 +7,17 @@ quando temos apenas if - estrutura condiocnal simples
 quando temos else - estrutura condicional composta
 """
 
-nome = input('Qual é seu nome? ').capitalize()
-if nome == 'Paulo':
-    print('Que belo nome!')
-else:
-    print('Seu nome é normal!')
-print(f'Bom dia {nome}!', )
+# nome = input('Qual é seu nome? ').capitalize()
+# if nome == 'Paulo':
+#     print('Que belo nome!')
+# else:
+#     print('Seu nome é normal!')
+# print(f'Bom dia {nome}!', )
+
+################################################################
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print(f'Sua média foi {media:.1f}')
